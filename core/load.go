21d96900e1918d1268af23d6951b5334becfc3e1
Qiu Yuzhou <qbowater@gmail.com>
2021-12-14 19:46:04 +0800

    docs: more comment for exported type and function

--- core/load.go
+++ core/load.go
@@ -2,6 +2,12 @@ package core
 
 // The iterator interface for loading policies and role mappings.
 type LoadIterator interface {
+	// Next returns next policy or role mapping.
+	//
+	// The `key` may be `p` or a role mapping key like `g`.
+	// If `ok` is false. There is no more data or an error.
 	Next() (ok bool, key string, vals []string)
+
+	// Error returns the error that has occurred in `Next()` calling.
 	Error() (err error)
 }
